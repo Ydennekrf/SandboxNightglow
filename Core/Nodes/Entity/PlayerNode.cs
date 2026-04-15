2400545310b67a9e0e5dc145f2bd6a43ce1b67df
fraser kennedy <ydennekrf@gmail.com>
2026-04-15 15:17:00 -0400

    Add minimal Core FSM Run locomotion state

--- Core/Nodes/Entity/PlayerNode.cs
+++ Core/Nodes/Entity/PlayerNode.cs
@@ -46,6 +46,7 @@ namespace ethra.V1
 			float dt = (float)delta;
 
 			_player.MoveInput = Input.GetVector("Left", "Right", "Up", "Down");
+			_player.RunPressed = Input.IsActionPressed("Run");
 			_player.Tick(dt);
 
 			Velocity = _player.DesiredVelocity;
