b8f81e0eae4105c948a524069fb4ee2b247d184b
fraser kennedy <ydennekrf@gmail.com>
2026-04-17 15:38:08 -0400

    Implement phase 1 crafting item flow and pickup

---
+++ Core/Inventory/Scripts/BasicInventoryItem.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace ethra.V1
+{
+    public class BasicInventoryItem : InventoryItem
+    {
+        public BasicInventoryItem(
+            int id,
+            string name,
+            int value,
+            string description,
+            string rarity,
+            List<ItemEffects> effects = null,
+            string category = "",
+            string subtype = "",
+            int maxStack = 99)
+            : base(id, name, value, description, rarity, effects, category, subtype, maxStack)
+        {
+        }
+    }
+}
--- Core/Inventory/Scripts/InventoryItem.cs
+++ Core/Inventory/Scripts/InventoryItem.cs
@@ -1,39 +1,60 @@
 using System.Collections.Generic;
-using System.Linq;
 using Godot;
 
 namespace ethra.V1
 {
     public abstract class InventoryItem : IInventoryItem, IResolveable
     {
-        private int _id;
-        private string _name;
-        private int _value;
-        private string description;
-        private string _rarity;
-        private List<ItemEffects> _effects;
-        private int _resolveOrder = 15;
-        private Entity _owner;
+        protected int _id;
+        protected string _name;
+        protected int _value;
+        protected string _description;
+        protected string _rarity;
+        protected string _category;
+        protected string _subtype;
+        protected int _maxStack = 99;
+        protected List<ItemEffects> _effects;
+        protected int _resolveOrder = 15;
+        protected Entity _owner;
 
         public int Id => _id;
         public string Name => _name;
         public int Value => _value;
-        public string Description => description;
+        public string Description => _description;
         public string Rarity => _rarity;
+        public string Category => _category;
+        public string Subtype => _subtype;
+        public int MaxStack => _maxStack;
         public List<ItemEffects> Effects => _effects;
         public Entity Owner => _owner;
 
         public int ResolveOrder => _resolveOrder;
 
+        protected InventoryItem()
+        {
+            _effects = new List<ItemEffects>();
+        }
 
+        protected InventoryItem(int id, string name, int value, string description, string rarity, List<ItemEffects> effects = null, string category = "", string subtype = "", int maxStack = 99)
+        {
+            _id = id;
+            _name = name;
+            _value = value;
+            _description = description;
+            _rarity = rarity;
+            _category = category;
+            _subtype = subtype;
+            _maxStack = maxStack > 0 ? maxStack : 99;
+            _effects = effects ?? new List<ItemEffects>();
+        }
 
         public ItemInfo GetInfo()
         {
-            string[] effectList = new string[12];
+            List<string> effectList = new List<string>();
 
-            foreach(ItemEffects effect in Effects)
+            foreach (ItemEffects effect in Effects)
             {
-                effectList.Append(effect.ToString());
+                effectList.Add(effect.ToString());
             }
             return new ItemInfo
             {
@@ -41,7 +62,7 @@ namespace ethra.V1
                 Value = Value.ToString(),
                 Description = Description,
                 Rarity = Rarity,
-                Effects = effectList
+                Effects = effectList.ToArray()
             };
         }
 
@@ -57,7 +78,8 @@ namespace ethra.V1
 
         public virtual void Use()
         {
-            GD.Print($"===Item Used=== : {Name} on {_owner.Name}");
+            string ownerName = _owner != null ? _owner.Name : "None";
+            GD.Print($"===Item Used=== : {Name} on {ownerName}");
         }
     }
 }
